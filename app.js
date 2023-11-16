aea6fe6e108a9878e7190934b635c435069aaf1d
Bradley Legge <bmlegge12@gmail.com>
2023-11-15 21:34:53 -0500

    Add active highlight on click

--- app.js
+++ app.js
@@ -13,16 +13,20 @@ imageProduct4.addEventListener('click', getProduct4)
 
 function getProduct1(){
     mainShoeImage.src = 'images/image-product-1.jpg'
+    imageProduct1.classList.add('shoe-image-click')
 }
 
 function getProduct2(){
     mainShoeImage.src = 'images/image-product-2.jpg'
+    imageProduct2.classList.add('shoe-image-click')
 }
 
 function getProduct3(){
     mainShoeImage.src = 'images/image-product-3.jpg'
+    imageProduct3.classList.add('shoe-image-click')
 }
 
 function getProduct4(){
     mainShoeImage.src = 'images/image-product-4.jpg'
+    imageProduct4.classList.add('shoe-image-click')
 }
